4e4a28ca66bea9cc539dfdd7ea193991c7eeb8b8
osyakan <kan86yeaoh@gmail.com>
2025-03-21 15:19:57 +0900

    fix packege.json

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/components/ClientHeader.jsx -> ./Header\":{\"id\":\"app/components/ClientHeader.jsx -> ./Header\",\"files\":[\"static/chunks/_app-pages-browser_app_components_Header_js.js\"]}}"
+self.__REACT_LOADABLE_MANIFEST='{"app/components/ClientHeader.jsx -> ./Header":{"id":8023,"files":["static/chunks/965-ab61a3687ddd5d89.js","static/chunks/23.6ad78433af988664.js"]}}';
